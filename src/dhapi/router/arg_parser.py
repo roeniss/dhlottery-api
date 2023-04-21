6bfae2182b382a6e4823cd1c4e8628ed441c13aa
Roeniss Moon <roeniss2@gmail.com>
2023-04-22 01:16:05 +0900

    bump version (1.2.2 -> 1.3.1)

--- src/dhapi/router/arg_parser.py
+++ src/dhapi/router/arg_parser.py
@@ -15,7 +15,7 @@ class HelpOnErrorParser(argparse.ArgumentParser):
 class ArgParser:
     def __init__(self):
         parser = HelpOnErrorParser(description="동행복권 비공식 API", formatter_class=argparse.RawTextHelpFormatter)
-        parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.2.2")
+        parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.3.1")
 
         command_subparser = parser.add_subparsers(title="명령어 구분", dest="command", required=True)
 
