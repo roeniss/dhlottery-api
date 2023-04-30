1b83970c420cbeb6e845accbc0a24f8cccac83e1
Roeniss Moon <roeniss2@gmail.com>
2023-04-30 21:04:08 +0900

    version 파일 분리, 의존성 설치하도록 setup 수정

--- setup.py
+++ setup.py
@@ -2,47 +2,47 @@ import pathlib
 
 from setuptools import find_packages, setup
 
-here = pathlib.Path(__file__).parent.resolve()
+HERE = pathlib.Path(__file__).parent.resolve()
 
-long_description = (here / "README.md").read_text(encoding="utf-8")
+
+def _get_dependencies():
+    with open(HERE + "/requirements.txt") as f:
+        return f.read().splitlines()
+
 
 setup(
     name="dhapi",
-    version="1.3.1",
-    description="DongHaeng Lottery Unofficial API",
-    long_description=long_description,
+    description="동행복권 비공식 API",
+    version=(HERE / "VERSION").read_text(encoding="utf-8"),
+
+    long_description=(HERE / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
+
     url="https://github.com/roeniss/dhlottery-api",
+    project_urls={
+        "Bug Reports": "https://github.com/roeniss/dhlottery-api/issues",
+        "Source": "https://github.com/roeniss/dhlottery-api/",
+    },
     author="Roeniss Moon",
     author_email="roeniss2@gmail.com",
+
     classifiers=[
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
-        "Environment :: MacOS X",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python",
     ],
-    keywords="api,korean,donghaeng,lottery",
+    keywords="donghaeng,lottery,lotto645,api,korean",
+
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.6, <4",
+    python_requires=">=3.8, <4",
+
     entry_points={
         "console_scripts": [
             "dhapi=dhapi.main:main",
         ],
     },
-    project_urls={
-        "Bug Reports": "https://github.com/roeniss/dhlottery-api/issues",
-        "Source": "https://github.com/roeniss/dhlottery-api/",
-    },
+
+    install_requires=_get_dependencies(),
 )
