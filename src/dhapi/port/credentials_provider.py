f18ea8ca0f990ad931be70392769f0868b1e7c24
southglory <devramyun@gmail.com>
2024-10-12 14:14:16 +0900

    Update credentials_provider.py
    프로필 저장하지 않고 매번 아이디, 비번 입력해서 쓰도록 수정

--- src/dhapi/port/credentials_provider.py
+++ src/dhapi/port/credentials_provider.py
@@ -1,8 +1,4 @@
 import logging
-import os
-
-import tomli
-import tomli_w
 
 from dhapi.domain.user import User
 
@@ -11,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 class CredentialsProvider:
     def __init__(self, profile_name):
-        self._path = os.path.expanduser("~/.dhapi/credentials")
+        # self._path = os.path.expanduser("~/.dhapi/credentials")
         self._credentials = self._get_credentials(profile_name)
 
     def _get(self, key):
@@ -23,57 +19,13 @@ class CredentialsProvider:
         return User(self._get("username"), self._get("password"))
 
     def _get_credentials(self, profile_name):
-        try:
-            _ = self._read_credentials_file(profile_name)
-        except FileNotFoundError:
-            print(f"❌ {self._path} 파일을 찾을 수 없습니다. 파일을 생성하고 프로필을 추가하시겠습니까? [Y/n] ", end="")
-            answer = input().strip().lower()
-            if answer in ["y", "yes", ""]:
-                print("📝 입력된 프로필 이름을 사용하시겠습니까? [Y/n]", end="")
-                answer = input().strip().lower()
-                if answer in ["y", "yes", ""]:
-                    self._add_credentials(profile_name)
-                else:
-                    print("📝 프로필 이름을 입력하세요: ", end="")
-                    profile_name = input().strip()
-                    self._add_credentials(profile_name)
-            else:
-                raise FileNotFoundError(f"{self._path} 파일을 찾을 수 없습니다.")
-
-        credentials = self._read_credentials_file(profile_name)
-
-        if credentials is None:
-            print(f"❌'{profile_name}' 프로필을 찾지 못했습니다. 추가하시겠습니까? [Y/n] ", end="")
-            answer = input().strip().lower()
-            if answer in ["y", "yes", ""]:
-                self._add_credentials(profile_name)
-                credentials = self._read_credentials_file(profile_name)
-                return credentials
-            raise ValueError(f"{self._path} 파일에서 '{profile_name}' 프로필을 찾지 못했습니다.")
-
-        return credentials
-
-    def _read_credentials_file(self, profile_name):
-        with open(self._path, "r", encoding="UTF-8") as f:
-            file = f.read()
-        config = tomli.loads(file)
-        credentials = config.get(profile_name)
-        return credentials
-
-    def _add_credentials(self, profile_name):
+        print(f"📝 프로필 이름: '{profile_name}'")
+        
+        # Always ask for user credentials
         print("📝 사용자 ID를 입력하세요: ", end="")
         user_id = input().strip()
         print("📝 사용자 비밀번호를 입력하세요: ", end="")
         user_pw = input().strip()
 
-        doc = {profile_name: {"username": user_id, "password": user_pw}}
-
-        with open(self._path, "r", encoding="UTF-8") as f:
-            file = f.read()
-        config = tomli.loads(file)
-
-        doc.update(config)
-
-        with open(self._path, "wb") as f:
-            tomli_w.dump(doc, f)
-            f.close()
+        credentials = {"username": user_id, "password": user_pw}
+        return credentials
